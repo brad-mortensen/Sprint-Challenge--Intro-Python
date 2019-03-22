72b7f09e208fb2277387147710c6ae272ec5cfc7
Brad Mortensen <b.mort86@gmail.com>
2019-03-22 12:20:07 -0600

    updates comp.py to be cleaner

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -9,7 +9,7 @@ class Human:
         self.age = age
 
     def __repr__(self):
-        return f"<Human: {self.name}, {self.age}>"
+        return f"Human: {self.name}, {self.age}"
 
 
 humans = [
@@ -27,49 +27,48 @@ humans = [
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
-print("Starts with D:")
 a = [p.name for p in humans if p.name[0] == 'D']
-print(a)
+print(f"Starts with D: {a}")
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
-print("Ends with e:")
+
 b = [p.name for p in humans if p.name[-1] == 'e']
-print(b)
+print(f"Ends with e: {e}")
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
-print("Starts between C and G, inclusive:")
+
 c = [p.name for p in humans if ord(
     p.name[0]) in range(ord('C'), ord('G') + 1)]
-print(c)
+print(f"Name starts between C and G inclusive: {c}")
 
 # Write a list comprehension that creates a list of all the ages plus 10.
-print("Ages plus 10:")
+
 d = [(p.age + 10) for p in humans]
-print(d)
+print(f"Ages plus 10: {d}")
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
-print("Name hyphen age:")
+
 e = [f'{p.name}-{p.age}' for p in humans]
-print(e)
+print(f"Name hyphen age: {e}")
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
-print("Names and ages between 27 and 32:")
+
 f = [(p.name, p.age)for p in humans if p.age in range(27, 33)]
-print(f)
+print(f"Names and ages 27-32: {f}")
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names capitalized and the ages with 5 added tothem.
 # The "humans" list should be unmodified.
-print("All names capitalized:")
+
 g = [Human(p.name.upper(), p.age + 5) for p in humans]
-print(g)
+print(f"All names capitalized: {g}")
 
 # Write a list comprehension that contains the square root of all the ages.
-print("Square root of ages:")
+
 h = [math.sqrt(p.age) for p in humans]
-print(h)
+print(f"Square root of ages: {h}")
